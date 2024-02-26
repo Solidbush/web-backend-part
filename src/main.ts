ca5cdccae247a1b078025fa1b839b501f5228993
Volirvag <solidbush@mail.ru>
2024-02-26 12:18:15 +0300

    Implemented cors rules

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {AppModule} from "./app.module";
 async function start() {
   const PORT = process.env.PORT || 3080;
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({origin: '*'})
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
